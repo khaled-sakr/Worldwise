455ff24ecae5f7fc8f346affd0dcd1f571c3efdc
Khaled Sakr <136926417+khaled-sakr@users.noreply.github.com>
2023-09-08 02:53:43 +0300

    first commit

---
+++ src/components/Practising.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+function Practising() {
+  // useEffect{async function blas() {
+  //     const res=await fetch(`http`)
+  //     const data=await res.json(),
+  //     setCity(data)
+
+  // }
+  // blas()
+  // ,[]}
+
+  return <div></div>;
+}
+
+export default Practising;
